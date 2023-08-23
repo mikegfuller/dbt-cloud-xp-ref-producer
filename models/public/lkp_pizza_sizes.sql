4b4ed482e204582cb91fcc06ecbbac01f1bff12f
Mike Fuller <mike.fuller@fishtownanalytics.com>
2023-08-23 22:00:08 +0000

    add a public model

---
+++ models/public/lkp_pizza_sizes.sql
@@ -0,0 +1,7 @@
+select 'xl' as pie_size, 16 as inches
+union all
+select 'l' as pie_size, 14 as inches
+union all
+select 'm' as pie_size, 12 as inches
+union all
+select 's' as pie_size, 10 as inches
